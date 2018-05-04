dd72e865c650d3a59bc0746f34e9cca760cc53bc
GdHadi <igede.nurhadi@gmail.com>
2018-05-05 00:25:01 +0800

    first commit and push

---
+++ handle-js/src/main/java/selenium_js/handle/App.java
@@ -0,0 +1,13 @@
+package selenium_js.handle;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
